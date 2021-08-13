b108035e4030b234b04dad5e968fa84cadb39970
unknown <madisettysaidivya22@gmail.com>
2021-08-13 14:26:04 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def divya(request):
+    return render(request,'nani.html')
+
+def harshad(request):
+    return render(request,'harshad.html')    
